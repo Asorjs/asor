5c8830dff77e9fb4b8c3cbab22e5a8d086d86df9
juancristobalgd1 <juancristobalgd1@gmail.com>
2024-10-17 17:29:16 +0200

    update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { start, stop } from "./root.js";
-import { directive } from "./directives.js";
+import { directive, getDirectives } from "./directives.js";
 import { dispatchGlobal, dispatchSelf, on } from "./utils/events.js";
 import { onDataChange } from "./features/supportSubscribers.js";
 import { evaluateInContext as evaluate} from "./features/supportEvaluateExpression.js";
@@ -7,6 +7,10 @@ import { handleStore as store } from "./features/supportStore.js";
 import { registerComponent as component } from "./features/supportComponents.js";
 import { mount } from "./directives.js";
 import { handleError  as error, warn } from "./utils/logger.js";
+import { debounce } from "./utils/debounce.js";
+import { throttle } from "./utils/throttle.js";
+import { updateDOM } from "./features/supportUpdateDom.js";
+
 import "./directives/index.js";
 
 const Asor = {
@@ -17,11 +21,15 @@ const Asor = {
     store,
     start,
     mount,
+    debounce,
+    throttle,
     evaluate,
     directive,
     component,
+    updateDOM,
     onDataChange,
     dispatchSelf,
+    getDirectives,
     dispatchGlobal,
 };
 
